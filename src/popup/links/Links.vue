e52dfda557e0f5f9fa645bfdbea1868261db7c15
andreyk <andreyk@jfrog.com>
2023-01-03 21:29:26 +0200

    finish with basic code

---
+++ src/popup/links/Links.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Links</div>
+  <a href="www.google.com">Google</a>
+</template>
---
+++ src/popup/popupApp/PopupApp.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { storageDemo } from "~/logic/storage";
+
+function openOptionsPage() {
+  browser.runtime.openOptionsPage();
+}
+const clickMe = () => {
+  console.log("1");
+};
+</script>
+
+<template>
+  <main class="w-[300px] px-4 py-5 text-center text-gray-700">
+    <Logo />
+    <div>Popup</div>
+    <SharedSubtitle />
+
+    <button class="btn mt-2" @click="openOptionsPage">Open Options</button>
+    <div class="mt-2">
+      <span class="opacity-50">Storage:</span> {{ storageDemo }}
+      <button type="button" class="btn mt-2" @click="clickMe">Click Me!</button>
+    </div>
+  </main>
+</template>
